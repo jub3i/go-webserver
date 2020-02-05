49ed1fcba5336a084dee6bd12e02ea564e862989
jub3i <anon66@gmail.com>
2020-02-05 16:38:30 +0200

    use justinas/alice for middleware

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/jub3i/go-webserver
 
 go 1.13
 
-require github.com/joho/godotenv v1.3.0
+require (
+	github.com/joho/godotenv v1.3.0
+	github.com/justinas/alice v1.2.0
+)
--- routes/routes.go
+++ routes/routes.go
@@ -3,6 +3,7 @@ package routes
 import (
 	c "github.com/jub3i/go-webserver/controllers"
 	mw "github.com/jub3i/go-webserver/middleware"
+	"github.com/justinas/alice"
 	"net/http"
 )
 
@@ -10,7 +11,7 @@ var Mux *http.ServeMux
 
 func init() {
 	Mux = http.NewServeMux()
-	Mux.Handle("/bye", mw.ReqLog(c.Handler{Msg: "BYE"}))
-	Mux.Handle("/hi", mw.ReqLog(c.Handler{Msg: "HI"}))
-	Mux.Handle("/deny", mw.ReqLog(mw.IsAdmin(c.Handler{Msg: "DENY"})))
+	Mux.Handle("/bye", alice.New(mw.ReqLog).Then(c.Handler{Msg: "BYE"}))
+	Mux.Handle("/hi", alice.New(mw.ReqLog).Then(c.Handler{Msg: "HI"}))
+	Mux.Handle("/deny", alice.New(mw.ReqLog, mw.IsAdmin).Then(c.Handler{Msg: "DENY"}))
 }
